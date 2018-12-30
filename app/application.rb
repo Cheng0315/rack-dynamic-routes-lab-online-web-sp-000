1ee1714f5f96096bc55f5ba7758526ef1f32c63f
Cheng Vue <cvu3@ucsc.edu>
2018-12-30 06:48:34 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,9 +1,13 @@
 class Application
-
+  @@item = []
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
+    if req.path.match("/items/")
+      item = req.path.split.last
+    else
 
+    end
   end
 end
