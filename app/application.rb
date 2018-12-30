bc52c478ded774632192b3c94e2b2312e74086c8
Cheng Vue <cvu3@ucsc.edu>
2018-12-30 07:15:15 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,7 +6,7 @@ class Application
     req = Rack::Request.new(env)
 
     if req.path.match("/items/")
-      item_name = req.path.scan(/(?<=items\/)(.*)(?=\/.+)/)
+      item_name = req.path.scan(/(?<=items\/)(.*)(?=\/.+)/).first
 
       if @@items.find {|item| item.name == item_name}
         resp.write @@items.find {|item| item.name == item_name}.price
